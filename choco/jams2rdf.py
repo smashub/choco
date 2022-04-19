0a60316f302e23efe098c048c18c52dc95b392a1
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2022-04-19 12:07:09 +0100

    working converter

--- choco/jams2rdf.py
+++ choco/jams2rdf.py
@@ -1,23 +1,42 @@
 import subprocess
 from rdflib import Graph
-import argparse
+import sys
+import os
 
-parser = argparse.ArgumentParser(description='Converts JAMS (JSON) files into RDF.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+query_template = "queries/jams.rq"
+query_current = "queries/current.rq"
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+def jams2rdf(input, output=None, outformat='turtle'):
+    with open(query_template, 'r') as r:
+        query_for_file =  r.read().replace("%FILEPATH%", input)
+        with  open(query_current, 'w') as w:
+            w.write(query_for_file)
 
-def jams2rdf(input, output):
-    out = subprocess.check_output(["java", "-jar", "bin/sparql-anything-0.6.0.jar", "-q", "queries/jams.rq"])
+    out = subprocess.check_output(["java", "-jar", "bin/sparql-anything.jar", "-q", query_current])
     
     g = Graph()
     g.parse(out)
     
-    print(g.serialize(format='turtle'))
+    if output:
+        with open(output, 'w') as wo:
+            wo.write(g.serialize(format=outformat))
+    else:
+        print(g.serialize(format=outformat))
 
-jams2rdf()
+    os.remove(query_current)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Usage: {0} <input file> [<output file>]".format(sys.argv[0]))
+        exit(2)
+    elif len(sys.argv) == 2:
+        # Conversion to stdout 
+        infilename = sys.argv[1]
+        jams2rdf(os.path.abspath(infilename))
+    elif len(sys.argv) == 3:
+        # Conversion to file
+        infilename = sys.argv[1]
+        outfilename = sys.argv[2]
+        jams2rdf(os.path.abspath(infilename), outfilename)
+
+    exit(0)
