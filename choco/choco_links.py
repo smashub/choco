cf81ea3526a3bf0839e356a8abb13a0c0e7fed1d
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2022-04-28 15:20:25 +0100

    output links file

--- choco/choco_links.py
+++ choco/choco_links.py
@@ -16,7 +16,7 @@ musicbrainz_prefix = "https://musicbrainz.org/recording/"
 owl_prefix = "http://www.w3.org/2002/07/owl#"
 
 
-def midi_choco_links(midi_path, jams_path):
+def midi_choco_links(midi_path, jams_path, links_outfile):
     links = Graph()
 
     abs_midi_path = os.path.abspath(midi_path)
@@ -63,7 +63,7 @@ def midi_choco_links(midi_path, jams_path):
                         pass
 
     print("Writing inherited links from JAMS.file_metadata.identifiers...")
-    with open('links.nt', 'w') as linksfile:
+    with open(links_outfile, 'w') as linksfile:
         linksfile.write(links.serialize(format='nt'))
 
     links = Graph()
@@ -79,7 +79,7 @@ def midi_choco_links(midi_path, jams_path):
                 p = URIRef(owl_prefix + 'sameAs')
                 o = URIRef(choco_prefix + j['id'].replace(" ", "_"))
                 links.add((s,p,o))
-                with open('links.nt', 'a') as linksfile:
+                with open(links_outfile, 'a') as linksfile:
                     linksfile.write(links.serialize(format='nt'))
                 links = Graph()
                 # time.sleep(1)
@@ -88,10 +88,10 @@ def midi_choco_links(midi_path, jams_path):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: {0} <midi file path> <jams file path>".format(sys.argv[0]))
+    if len(sys.argv) < 4:
+        print("Usage: {0} <midi file path> <jams file path> <links outfile>".format(sys.argv[0]))
         exit(2)
 
-    midi_choco_links(sys.argv[1], sys.argv[2])
+    midi_choco_links(sys.argv[1], sys.argv[2], sys.argv[3])
 
     exit(0)
