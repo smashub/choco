100372c0271a98606eee9de3c3ba9d3fb14f21c0
andreamust <andrea.poltronieri21@gmail.com>
2022-06-07 09:37:13 +0000

    reformat kg-generation module

--- choco/kg-generation/jams2rdf.py
+++ choco/kg-generation/jams2rdf.py
@@ -12,9 +12,9 @@ query_test = "queries/jams_ontology.sparql"
 choco_namespace = "http://purl.org/choco/data/"
 
 
-def jams2rdf(input, output=None, outformat='ttl'):
+def jams2rdf(input_file: str, output=None, output_format: str = 'ttl'):
     with open(query_test, 'r') as r:
-        query_for_file = r.read().replace("%FILEPATH%", input)
+        query_for_file = r.read().replace("%FILEPATH%", input_file)
         with open(query_current, 'w') as w:
             w.write(query_for_file)
 
@@ -25,14 +25,14 @@ def jams2rdf(input, output=None, outformat='ttl'):
         g.parse(out)
     except CalledProcessError as e:
         print(e)
-        print("Output graph is empty for {}".format(input))
+        print("Output graph is empty for {}".format(input_file))
         pass
 
     # Replace root node name
 
     foo_uri = URIRef(choco_namespace + 'foo')
-    jams_file = input.split('/')[-1].split('.')[0]
-    jams_collection = input.split('/')[6]
+    jams_file = input_file.split('/')[-1].split('.')[0]
+    jams_collection = input_file.split('/')[6]
     resource_uri = URIRef(choco_namespace + jams_collection + '/' + jams_file)
     for s, p, o in g.triples((foo_uri, None, None)):
         g.add((resource_uri, p, o))
@@ -40,9 +40,9 @@ def jams2rdf(input, output=None, outformat='ttl'):
 
     if output:
         with open(output, 'w') as wo:
-            wo.write(g.serialize(format=outformat))
+            wo.write(g.serialize(format=output_format))
     else:
-        print(g.serialize(format=outformat))
+        print(g.serialize(format=output_format))
 
     os.remove(query_current)
 
