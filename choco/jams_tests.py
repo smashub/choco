a2493418276a67c60e9a2e8b99cc881c50b18d99
Jacopo de Berardinis <jonnybluesman@gmail.com>
2022-06-20 16:20:09 +0100

    testing scripts for silver set creation up

--- choco/jams_tests.py
+++ choco/jams_tests.py
@@ -4,9 +4,16 @@ Utilities for JAMS-driven testing and entry point for running tests.
 """
 import re
 import os
+import logging
+import argparse
+from attr import validate
 
 import jams
 import pandas as pd
+
+from utils import is_dir, create_dir
+
+logger = logging.getLogger("choco.tests")
 
 KEEP_STRATEGIES = ["last_n", "first_n"]  # for skimming test JAMS
 
@@ -71,7 +78,7 @@ def generate_silver_jams(in_jams: jams.JAMS, keep_n=6, keep_loc="first_n"):
     # Selection of the annotation namespaces: kept in the same order
     new_jams = jams.JAMS(file_metadata=in_jams.file_metadata,
         sandbox=in_jams.sandbox)  # spawn JAMS with no annotations
-    
+
     for annotation in in_jams.annotations:
         if re.search("chord|key", annotation.namespace) is not None:
 
@@ -87,8 +94,8 @@ def generate_silver_jams(in_jams: jams.JAMS, keep_n=6, keep_loc="first_n"):
     return new_jams
 
 
-def generate_partition_testset(partition_path: str, n_sample : int, seed=1234,
-    keep_n=5):
+def generate_partition_testset(partition_path: str, n_sample : int, keep_n=5,
+    seed=1234):
     """
     Generate a full test set for a given ChoCo partition, made of a selection of
     silver JAMS (to be analysed by the annotators) together with a test CSV. 
@@ -114,21 +121,26 @@ def generate_partition_testset(partition_path: str, n_sample : int, seed=1234,
 
     """
     meta_path = os.path.join(partition_path, "choco/meta.csv")
-    test_meta = generate_partition_testset(
+    test_path = create_dir(os.path.join(partition_path, "test"))
+    test_meta = select_partition_testset(
         meta_path, n_sample=n_sample, seed=seed)
+    test_meta["silver_path"] = None
 
     for idx, test_record in test_meta.iterrows():
-
+        # Retriving the current ChoCo's JAMS and renaming the silver
         jams_path = test_record["jams_path"][3:]
-
+        silver_path = os.path.join(
+            test_path, test_record["id"] + "_silver.jams")
+        # Generating the silver JAMS based on the given specifications
         choco_jams = jams.load(jams_path)
         silver_jams = generate_silver_jams(
             choco_jams, keep_n=keep_n,
             keep_loc=test_record["keep"])
-        
-        # TODO Save JAMS to disk
-        # TODO Add silver path in test meta
-    
+        # Save the silver JAMS to the testset partition
+        silver_jams.save(silver_path, strict=False)
+        test_meta.loc[idx, "silver_path"] = silver_path
+
+    test_meta.to_csv(os.path.join(test_path, "test_meta.csv"), index=False)
     return test_meta
 
 
@@ -152,3 +164,53 @@ def compare_jams(choco_jams: jams.JAMS, gtruth_jams: jams.JAMS):
 
     """
     raise NotImplementedError
+
+
+
+def main():
+    """
+    Entry point to read the arguments and call the conversion scripts.
+    """
+    parser = argparse.ArgumentParser(
+        description='Testing scripts for the JAMification of ChoCo partitions.')
+
+    parser.add_argument('cmd', choices=["create", "test"],
+                        help='Either `create` for generating the test samples'
+                             ' or `test` for running the JAMS-based tests.')
+    parser.add_argument('partition_dir', type=lambda x: is_dir(parser, x),
+                        help='Path to the directory of the ChoCo partition.')
+    # This is useful to avoid the `last_n` strategy from being selected if the
+    # content of the partition is symbolic music, hence the 'expansion' of the
+    # score into the performed score will not complicate the evaluation process.
+    parser.add_argument('type', type=str, choices=["audio", "score"],
+                        help='Type of music content in the collection.')
+
+    parser.add_argument('--n_samples', action='store', type=int, default=4,
+                        help='Number of test samples to draw from the partition'
+                             ', create the test metadata, and skim the JAMS.')
+    parser.add_argument('--keep_n', action='store', type=int, default=5,
+                        help='Number of chord/key observations per annotation'
+                             ' that will be retained for the evaluation.')
+    parser.add_argument('--seed', action='store', type=int, default=1234,
+                        help='A random seed for reproducible sampling.')
+
+    args = parser.parse_args()
+
+    if args.cmd == "create":
+        print("Creating partition test set: this may take a while...")
+        generate_partition_testset(
+            partition_path=args.partition_dir,
+            n_sample=args.n_samples,
+            keep_n=args.keep_n,
+            seed=args.seed
+        )
+        print(f"Test set for partition {args.partition_dir} is ready.")
+
+    else:  # XXX this is not ready, yet
+        raise NotImplementedError
+    
+    
+
+
+if __name__ == "__main__":
+    main()
