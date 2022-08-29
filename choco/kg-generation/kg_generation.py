d20c9fc36b9827a425637dcc620e227a76331d95
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-08-29 18:36:29 +0200

    parallelised kg_conversion

--- choco/kg-generation/kg_generation.py
+++ choco/kg-generation/kg_generation.py
@@ -1,10 +1,67 @@
+from argparse import ArgumentParser
 from pathlib import Path
 from typing import Union
-from argparse import ArgumentParser
 
 import pandas as pd
+from joblib import Parallel, delayed
 
 from jams2rdf import jams2rdf
+
+
+def parallel_jams2rdf(path: Union[str, Path],
+                      output_path: str,
+                      query_path: Union[str, Path],
+                      sparql_anything_path: Union[str, Path],
+                      metadata: pd.DataFrame,
+                      rdf_serialisation: str = 'TTL',
+                      handle_error: bool = False) -> None:
+    """
+
+    Parameters
+    ----------
+    path : Union[str, Path]
+        The path (either absolute or relative) of the JAMS files dataset
+        to be converted into RDF
+    output_path : Union[str, Path]
+        The path (either absolute or relative) to which the output file will be
+        saved
+    query_path : Union[str, Path]
+        The path (either absolute or relative) to the SPARQL Anything query
+    sparql_anything_path : Union[str, Path]
+        The path to the SPARQL Anything .jar file, which can be downloaded from
+        the SPARQL Anything GitHub repository
+    metadata : pd.DataFrame
+        A pandas DataFrame for storing metadata about the Knowledge Graph
+        generation
+    rdf_serialisation  : str
+        The RDF serialisation to be used for the output file. For the
+        serialisations available, please refer to the rdflib documentation
+    handle_error : bool
+        Defines whether to stop if an error ir raised or not.
+
+    Returns
+    -------
+    None
+    """
+    try:
+        file_path = Path(
+            output_path) / f'{path.stem}.{rdf_serialisation.lower()}'
+
+        file_metadata = jams2rdf(input_file=path,
+                                 output_path=file_path,
+                                 query_path=query_path,
+                                 sparql_anything_path=sparql_anything_path)
+
+        file_metadata = [path.stem, path, file_path].extend(file_metadata)
+        metadata.append(file_metadata)
+
+    except ValueError as ve:
+        if handle_error:
+            metadata.append([path.stem, 'error'])
+        else:
+            raise NameError(
+                f'The conversion of the file {path.name} raised an error:\n'
+                f'{ve}')
 
 
 def kg_generation(dataset_path: Union[str, Path],
@@ -46,26 +103,14 @@ def kg_generation(dataset_path: Union[str, Path],
     metadata = pd.DataFrame()
     pattern = 'jams-converted/*.jams' if only_converted else '*.jams'
 
-    for path in dataset_path.rglob(pattern):
-        try:
-            file_path = Path(
-                output_path) / f'{path.stem}.{rdf_serialisation.lower()}'
-
-            file_metadata = jams2rdf(input_file=path,
-                                     output_path=file_path,
-                                     query_path=query_path,
-                                     sparql_anything_path=sparql_anything_path)
-
-            file_metadata = [path.stem, path, file_path].extend(file_metadata)
-            metadata.append(file_metadata)
-
-        except ValueError as ve:
-            if handle_error:
-                metadata.append([path.stem, 'error'])
-            else:
-                raise NameError(
-                    f'The conversion of the file {path.name} raised an error:\n'
-                    f'{ve}')
+    Parallel(n_jobs=-1)(delayed(parallel_jams2rdf)(path,
+                                                   output_path,
+                                                   query_path,
+                                                   sparql_anything_path,
+                                                   metadata,
+                                                   rdf_serialisation,
+                                                   handle_error) for path in
+                        dataset_path.rglob(pattern))
 
     return metadata
 
@@ -111,28 +156,41 @@ def main() -> None:
                         default='TTL',
                         required=False)
     parser.add_argument('--only_converted',
-                        type=str,
+                        type=bool,
                         help='Defines whether to take into account only files'
                              'contained in the "converted" folders (True) or '
                              'all the JAMS files encountered (False).',
-                        default='TTL',
-                        required=True)
+                        default=True,
+                        required=False)
     parser.add_argument('--handle_error',
-                        type=str,
+                        type=bool,
                         help='Defines whether to stop if an error ir raised or '
                              'not.',
-                        default='TTL',
+                        default=False,
+                        required=False)
+    parser.add_argument('--save_metadata',
+                        type=bool,
+                        help='Defines whether to stop if an error ir raised or '
+                             'not.',
+                        default=True,
                         required=False)
 
     args = parser.parse_args()
 
-    kg_generation(dataset_path=args.dataset_path,
+    metadata = kg_generation(dataset_path=args.dataset_path,
                   output_path=args.output_path,
                   query_path=args.query_path,
                   sparql_anything_path=args.sparql_anything_path,
                   rdf_serialisation=args.rdf_serialisation,
                   only_converted=args.only_converted,
                   handle_error=args.handle_error)
+
+    if args.save_metadata:
+        metadata.to_csv(Path(args.output_path) / 'kg_generation_metadata.csv',
+                        index=False,
+                        header=['filename',
+                                'path',
+                                ])
 
 
 if __name__ == "__main__":
