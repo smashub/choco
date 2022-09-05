a0fd789aa719f203b9c364ca30513fe5cb08041a
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-09-05 02:16:04 +0200

    kg version 2 up

--- choco/kg-generation/jams2rdf.py
+++ choco/kg-generation/jams2rdf.py
@@ -132,7 +132,7 @@ def main() -> None:
 
 if __name__ == '__main__':
     # Test case
-    jams2rdf('examples/score_wiki.jams', 'examples/score_wiki.ttl',
+    jams2rdf('examples/audio_wiki.jams', 'examples/audio_wiki.ttl',
              'queries/jams_ontology.sparql',
              'bin/sa.jar')
 
