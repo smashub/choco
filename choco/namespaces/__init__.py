421e984b0d9381f685841e1e176f8fbec0d511bd
Jacopo de Berardinis <jonnybluesman@gmail.com>
2022-12-13 16:06:10 +0000

    timesig namespace added

--- choco/namespaces/__init__.py
+++ choco/namespaces/__init__.py
@@ -3,9 +3,13 @@ import jams
 
 ns_dir = os.path.dirname(os.path.abspath(__file__))
 
+# New chord namespaces
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_ireal.json"))
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_jparser_harte.json"))
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_jparser_functional.json"))
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_m21_leadsheet.json"))
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_m21_abc.json"))
 jams.schema.add_namespace(os.path.join(ns_dir, "chord_weimar.json"))
+
+# New symbolic namespaces
+jams.schema.add_namespace(os.path.join(ns_dir, "timesig.json"))
